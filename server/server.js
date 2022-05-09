4c7bc069497051fc83c926f711270914065751e6
andrew hunsicker <ahunsicker888@gmail.com>
2022-05-08 22:20:40 -0400

    tweaking html

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
 })
 
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const {seed} = require('./seed.js')
 const {SERVER_PORT} = process.env
@@ -26,6 +27,9 @@ const {
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/landing.html'))
+})
 app.use(express.json())
 app.use(cors())
 app.post(`/api/login`, login)
